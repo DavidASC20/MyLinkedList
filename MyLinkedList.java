909552060ad9774ccf5bceab4d4ccbe675bb34b0
David Chong <djchong24@gmail.com>
2021-01-10 20:41:47 -0500

    finished add withoput index

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,8 +7,25 @@ public class MyLinkedList{
     public int size(){
         return size;
     }
-    // public boolean add(String value);
-    // public boolean add(int index, String value);
+    public boolean add(String value){
+        Node x = new Node(value);
+        if(size == 0){
+            start = x;
+            end = x;
+        }else{
+            end.setNext(x);
+            x.setPrev(end);
+            end = x;
+        }
+    }
+    public boolean add(int index, String value){
+        Node x = new Node(value);
+        if(index == 0){
+            start.setPrev(x);
+            x.setNext(start);
+            start = x;
+        }else if(index ==)
+    }
     // public String get(int index);
     // public String set(int index, String value);
     // public String toString();
